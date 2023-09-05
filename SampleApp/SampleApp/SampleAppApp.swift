fc71a0f798cc11d13ebe541c22b9082c674d4c5c
Aldrian Kwantawijaya <aldrian.k@gdn-commerce.com>
2023-09-05 10:09:25 +0700

    Add sample project; Update gitignore

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Aldrian Kwantawijaya on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
